da01bcca90f0ee64dbc645a079239b1e0c546cc3
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-13 05:14:15 -0600

    Actualizacion 2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -8,9 +8,9 @@
     $statement->execute();
     $filas=$statement->rowCount();
     if($filas>0){
-        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+        echo "si";
     }else{
-        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/login.php");
+        echo "no";
     }
     //Funcion para el login
     function login($usuario,$contrasena)
