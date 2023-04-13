a943437cc0e9b17c638337f46cecb6986c35ed87
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-13 03:35:26 -0600

    Actualizacion 2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -6,7 +6,7 @@
     {
         global $pdo;
 
-        $sql = "SELECT * from usuarios WHERE id==1 ";
+        $sql = "SELECT * from usuarios WHERE 'id==1' ";
 
         $statements = $pdo->prepare($sql);
         
@@ -22,7 +22,7 @@
     function getlogin(){
         global $pdo;
 
-        $sql = "SELECT * from usuarios WHERE 'id=1'";
+        $sql = "SELECT * from usuarios WHERE 'id==1'";
 
         $statements=$pdo->prepare($sql);
 
