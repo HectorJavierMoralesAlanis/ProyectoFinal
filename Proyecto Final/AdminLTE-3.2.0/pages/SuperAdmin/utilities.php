54d6bf9e3fe631b64b1105a46c3e2b2c9c942767
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-13 03:22:48 -0600

    Actualizacion 2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -4,7 +4,6 @@
     function login($usuario,$contrasena)
     {
         global $pdo;
-        $boton = $_REQUEST['boton1'];
 
         $sql = "SELECT * from usuarios WHERE 'id==1' ";
 
@@ -12,7 +11,7 @@
         
         $statements-> execute();
         $results=$statements->fetchAll();
-        echo $boton;
+        echo $results['usuarios'];
         if($usuario == $results['usuarios']&&$contrasena==$results['contrasena']){
             return true;
         }else{
