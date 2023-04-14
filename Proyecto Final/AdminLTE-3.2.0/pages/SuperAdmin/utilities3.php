f0ce7a92a758733e55a89140fa388588be3fd12f
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-14 05:00:23 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities3.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities3.php
@@ -1,12 +1,9 @@
 <?php
     include_once('./conexion.php');
 
-    function getTienda(){
-        global $pdo;
-        $sql="SELECT * from tienda";
-        $statement=$pdo->prepare($sql);
-        $statement->execute();
-        $results=$statement->fetchAll();
-        return $results;
-    }
+    global $pdo;
+    $sql="SELECT * from tienda";
+    $statement=$pdo->prepare($sql);
+    $statement->execute();
+    $tiendas=$statement->fetchAll();
 ?>
