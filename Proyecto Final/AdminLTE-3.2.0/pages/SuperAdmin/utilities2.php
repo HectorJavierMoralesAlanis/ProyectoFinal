57460bc37bc6529f0bc5094a687d1d6f7f5ed877
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-14 01:16:40 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
@@ -5,17 +5,8 @@
         global $pdo;
         
         //Definir sql
-        $sql = "INSERT INTO tienda (nombre,estado) VALUES(:nombre,:estado)";
+        $sql = "INSERT INTO tienda  VALUES('$nombre','$estado')";
         $statement = $pdo->prepare($sql);
-
-        //Declaracion de las variables
-        $nombreT = "Walmart";
-        $estadoT = "Activo";
-
-        //Agregar los valores a la base
-        $statement->bindParam(':nombre',$nombreT);
-        $statement->bindParam(':estado',$estadoT);
-
         $statement->execute();
 
     }
