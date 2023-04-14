2d04b60701aa891a9d148abb31c4149ad3fb459e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-14 03:31:51 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
@@ -1,22 +1,25 @@
 <?php
     include_once('./conexion.php');
 
-    global $pdo;
+    if(!empty($_POST['nombre'])&&!empty($_POST['estado'])){
+        global $pdo;
         
-    $nombre=$_REQUEST['nombre'];
-    $estado=$_REQUEST['estado'];
-    $id=" ";
-    //Definir sql
-    $sql = "INSERT INTO tienda (id,nombre,estado)VALUES(:id,:nombre,:estado)";
-    $statement = $pdo->prepare($sql);
-    $statement->bindParam(":nombre",$nombre);
-    $statement->bindParam(":estado",$estado);
-    $statement->bindParam(":id",$id);
-        
-    if($statement->execute()){
-        header("Location: ./registro_tienda.php");
-    }else {
-        print("Error en la consulta");
+        $nombre=$_REQUEST['nombre'];
+        $estado=$_REQUEST['estado'];
+        $id=" ";
+        //Definir sql
+        $sql = "INSERT INTO tienda (id,nombre,estado)VALUES(:id,:nombre,:estado)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(":nombre",$nombre);
+        $statement->bindParam(":estado",$estado);
+        $statement->bindParam(":id",$id);
+            
+        if($statement->execute()){
+            header("Location: ./registro_tienda.php");
+        }else {
+            print("Error en la consulta");
+        }
+    }else{
+        print("Error");
     }
-
 ?>
