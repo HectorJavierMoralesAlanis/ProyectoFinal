5a895209d59ffc06af9b442331e6ec7d9ded6bcf
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-14 00:50:49 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
@@ -4,7 +4,7 @@
     function addTienda(){
         global $pdo;
         
-        $sql = "INSERT INTO tienda VALUES('1 ','Walmart','Activo')";
+        $sql = "INSERT INTO tienda VALUES('1','Walmart','Activo')";
 
         $statement = $pdo->prepare($sql);
         $statement->execute();
