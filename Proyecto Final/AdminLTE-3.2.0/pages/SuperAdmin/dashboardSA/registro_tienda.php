bafe24fde36e39fca9194c08229708eb88527623
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-13 23:11:45 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/registro_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/registro_tienda.php
@@ -2,7 +2,7 @@
     include_once("../utilities2.php");
 
     if(isset($_POST['nombre'])&&isset($_POST['estado'])){
-        echo "<h2 style='center'>Si tiene valor</h2>";
+        echo "<h2 style='float: right;'>Si tiene valor</h2>";
     }
 ?>
 <html>
