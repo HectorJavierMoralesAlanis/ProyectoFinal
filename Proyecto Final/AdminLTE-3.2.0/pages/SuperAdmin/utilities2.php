62c9b0acc15785d9f4b5683b2f771377a6eb05b8
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-14 03:22:52 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities2.php
@@ -1,21 +1,19 @@
 <?php
     include_once('./conexion.php');
 
-    function addTienda(){
-        global $pdo;
+    global $pdo;
         
-        $nombre=$_POST['nombre'];
-        $estado=$_POST['estado'];
+    $nombre=$_POST['nombre'];
+    $estado=$_POST['estado'];
 
-        //Definir sql
-        $sql = "INSERT INTO tienda (nombre,estado)VALUES(:nombre,:estado)";
-        $statement = $pdo->prepare($sql);
-        $statement->bindParam(":nombre",$nombre);
-        $statement->bindParam(":estado",$estado);
+    //Definir sql
+    $sql = "INSERT INTO tienda (nombre,estado)VALUES(:nombre,:estado)";
+    $statement = $pdo->prepare($sql);
+    $statement->bindParam(":nombre",$nombre);
+    $statement->bindParam(":estado",$estado);
         
-        if($statement->execute()){
-            header("Location: ./registro_tienda.php");
-        }else print("Error en la consulta");
+    if($statement->execute()){
+        header("Location: ./registro_tienda.php");
+    }else print("Error en la consulta");
 
-    }
 ?>
