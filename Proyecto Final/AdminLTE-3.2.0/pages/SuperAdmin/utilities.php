38dd301c1d2188fbcd2f9b685e13afbd78bfe6b3
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-16 07:12:50 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -42,17 +42,16 @@
             $estado=$_REQUEST['estado'];
 
             //Definir la consulata
-            $sql = "INSERT INTO tienda VALUES($_REQUEST[nombre],$_REQUEST[estado])";
+            $sql = "INSERT INTO tienda (nombre,estado)VALUES(:nombre,:estado)";
             $statement = $pdo->prepare($sql);
 
             //Se agregan a la consulta los valores de las variables
-            /*
             $statement->bindParam(":nombre",$nombre);
-            $statement->bindParam(":estado",$estado);*/
+            $statement->bindParam(":estado",$estado);
                 
             //Condicional donde si se ejecuta la consulta exitosa te redirecciona al dashboard y guarda los valores si es falsa se manda un mensaje de error
             if($statement->execute()){
-                header("http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/registro_tienda.php");
+                header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
             }else {
                 print("Error en la consulta");
             }
