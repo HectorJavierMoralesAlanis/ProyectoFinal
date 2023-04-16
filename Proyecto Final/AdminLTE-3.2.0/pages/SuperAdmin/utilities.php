3d540aecc5fcb41b319cf09cd4b59df2ab0697b2
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-16 06:56:06 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -35,25 +35,49 @@
 
     function addTienda(){
         global $pdo;
+if(!empty($_POST['nombre'])&&!empty($_POST['estado'])){
+        global $pdo;
 
-        //Variable para guardar los valores del nombre y el estado de la tienda
+        //Variables para guardar los valoes del nombre y el estado de la tienda
         $nombre=$_REQUEST['nombre'];
         $estado=$_REQUEST['estado'];
-            
-        //Definir la consulta
-        $sql = "INSERT INTO tienda (nombre,estado) VALUES (:nombre,:estado)";
-            $statement = $pdo->prepare($sql);
 
-        //Se agrega a la consulta los valores de las variables
+        //Definir la consulata
+        $sql = "INSERT INTO tienda (nombre,estado)VALUES(:nombre,:estado)";
+        $statement = $pdo->prepare($sql);
+
+        //Se agregan a la consulta los valores de las variables
         $statement->bindParam(":nombre",$nombre);
         $statement->bindParam(":estado",$estado);
-
+            
         //Condicional donde si se ejecuta la consulta exitosa te redirecciona al dashboard y guarda los valores si es falsa se manda un mensaje de error
         if($statement->execute()){
             header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
-        }else {        
+        }else {
             print("Error en la consulta");
         }
+    }if(!empty($_POST['nombre'])&&!empty($_POST['estado'])){
+        global $pdo;
+
+        //Variables para guardar los valoes del nombre y el estado de la tienda
+        $nombre=$_REQUEST['nombre'];
+        $estado=$_REQUEST['estado'];
+
+        //Definir la consulata
+        $sql = "INSERT INTO tienda (nombre,estado)VALUES(:nombre,:estado)";
+        $statement = $pdo->prepare($sql);
+
+        //Se agregan a la consulta los valores de las variables
+        $statement->bindParam(":nombre",$nombre);
+        $statement->bindParam(":estado",$estado);
+            
+        //Condicional donde si se ejecuta la consulta exitosa te redirecciona al dashboard y guarda los valores si es falsa se manda un mensaje de error
+        if($statement->execute()){
+            header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+        }else {
+            print("Error en la consulta");
+        }
+    }
     }
 
     function mostrarTienda(){
