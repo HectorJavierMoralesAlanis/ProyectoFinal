3bd2679d50745ac758afbae4144715f969049e93
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-16 06:51:58 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -41,7 +41,7 @@
         $estado=$_REQUEST['estado'];
             
         //Definir la consulta
-        $sql = "INSERT INTO tienda (nombre,estado) VALUES (:nombre,:estado)";
+        $sql = "INSERT INTO tienda (nombre,estado) VALUES (:nombre,:estad)";
             $statement = $pdo->prepare($sql);
 
         //Se agrega a la consulta los valores de las variables
