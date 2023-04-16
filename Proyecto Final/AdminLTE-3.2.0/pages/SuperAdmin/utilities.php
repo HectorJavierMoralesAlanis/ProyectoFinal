86a99fea8c69a8b363b0ed9081a6e7ce1b83ebcf
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-16 06:42:13 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/utilities.php
@@ -34,27 +34,25 @@
     };
 
     function addTienda(){
-        if(!empty($_REQUEST['nombre'])&&!empty($_REQUEST['estado'])){
-            global $pdo;
+        global $pdo;
 
-            //Variable para guardar los valores del nombre y el estado de la tienda
-            $nombre=$_REQUEST['nombre'];
-            $estado=$_REQUEST['estado'];
+        //Variable para guardar los valores del nombre y el estado de la tienda
+        $nombre=$_REQUEST['nombre'];
+        $estado=$_REQUEST['estado'];
             
-            //Definir la consulta
-            $sql = "INSERT INTO tienda (nombre) VALUES (:nombre,:estado)";
+        //Definir la consulta
+        $sql = "INSERT INTO tienda (nombre) VALUES (:nombre,:estado)";
             $statement = $pdo->prepare($sql);
 
-            //Se agrega a la consulta los valores de las variables
-            $statement->bindParam(":nombre",$nombre);
-            $statement->bindParam(":estado",$estado);
+        //Se agrega a la consulta los valores de las variables
+        $statement->bindParam(":nombre",$nombre);
+        $statement->bindParam(":estado",$estado);
 
-            //Condicional donde si se ejecuta la consulta exitosa te redirecciona al dashboard y guarda los valores si es falsa se manda un mensaje de error
-            if($statement->execute()){
-                header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
-            }else {        
-                print("Error en la consulta");
-            }
+        //Condicional donde si se ejecuta la consulta exitosa te redirecciona al dashboard y guarda los valores si es falsa se manda un mensaje de error
+        if($statement->execute()){
+            header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+        }else {        
+            print("Error en la consulta");
         }
     }
 
