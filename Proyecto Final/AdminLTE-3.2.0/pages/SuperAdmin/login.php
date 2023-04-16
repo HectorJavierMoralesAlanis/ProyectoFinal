922f0f6eb8b54f77f5ddbfdbd6e2752c500c884f
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-16 04:36:14 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/login.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/login.php
@@ -2,7 +2,7 @@
     echo 'hoola';
     include('./utilities.php');
     //login();
-    echo 'HOLA'.htmlspecialchars($_POST['nombre']);
+    echo 'HOLA'.htmlspecialchars($_POST["nombre"]);
     /*if(!empty($_POST['nombre'])&&!empty($_POST['contrasena'])){
         //login($_POST['nombre'],$_POST['contrasena']);
         login();
