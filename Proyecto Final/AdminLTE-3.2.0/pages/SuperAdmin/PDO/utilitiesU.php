ba6b356a5478f681a287760bd4dcc23c180bb6ba
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-17 04:36:38 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/PDO/utilitiesU.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/PDO/utilitiesU.php
@@ -1,14 +1,16 @@
 <?php
     require_once './DAO.php';
-    $dao = new DAO();
-    $consulta = "SELECT * FROM tienda";
 
-    try{
-        $datos=$dao->ejecutarConsulta($consulta);
-        if(isset($datos)&&!empty($datos)&&sizeof($datos)>0){
-            $tiendas=$datos;
+    function getTiendas(){
+        $dao= new DAO;
+        $consulta = "SELECT * FROM tienda";
+        try{
+            $datos=$dao->ejecutarConsulta($consulta);
+            if(isset($datos)&&!empty($datos)&&sizeof($datos)>0){
+                $tiendas=$datos;
+            }
+        }catch(Exception $ex){
+
         }
-    }catch(Exception $ex){
-
     }
 ?>
