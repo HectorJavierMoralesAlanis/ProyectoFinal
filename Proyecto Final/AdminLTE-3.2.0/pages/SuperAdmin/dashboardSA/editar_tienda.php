ee068e41cddee65c33a68142e030ec3b730aee8c
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 04:59:54 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -9,16 +9,13 @@
     if(isset($_POST['enviar'])){
         if(!empty($_POST['nombre'])&&!empty($_POST['estado'])){
             $dao2=new DAO();
-            echo $_GET['id'];
-            $consulta2="UPDATE tienda SET nombre = :nombre, estado = :estado "."WHERE id=:id";
-            
-            $parametros2= array("nombre"=>"$_POST[nombre]","estado"=>"$_POST[estado]");
+            $id2=$_GET['id'];
+            $consulta2="UPDATE tienda SET nombre = :nombre, estado = :estado "."WHERE id=:id2";
+            $parametros2= array("nombre"=>"$_POST[nombre]","estado"=>"$_POST[estado]","id2"=>$id2);
 
             $resultado=$dao2->insertarConsulta($consulta2,$parametros2);
             if($resultados>=0){
-
-                echo $_GET['id'];
-                //header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+                header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
             }else{
                 echo "error";
             }
