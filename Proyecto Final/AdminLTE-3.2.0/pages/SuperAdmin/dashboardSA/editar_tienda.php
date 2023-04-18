d96be56ba820a38515e5925cfafb9a5e845ecb4d
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 04:13:10 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -117,16 +117,17 @@
                                             <label>
                                                 Estado:
                                             </label>
-
+                                            <?php if($tiendas['estado']=='Activado'){?>
                                             <div class="custom-control custom-radio">
-                                                <input class="custom-control-input" type="radio" id="activo" name="estado" <?php if($tiendas['estado']=='Activado'){?>checked<?php  }?> >
+                                                <input class="custom-control-input" type="radio" id="activo" name="estado" checked> 
                                                 <label for="activo" class="custom-control-label">ACTIVADA</label>
                                             </div>
+                                            <?php }else if($tiendas['estado']=='Desactivado'){?>
                                             <div class="custom-control custom-radio">
-                                                <input class="custom-control-input custom-control-input-danger" type="radio" id="desactivado" name="estado">
+                                                <input class="custom-control-input custom-control-input-danger" type="radio" id="desactivado" name="estado" checked>
                                                 <label for="desactivado" class="custom-control-label">DESACTIVADA</label>
                                             </div>
-
+                                            <?php }?>
                                         </div>
                                     <?php }?>
                                     <div class="btn-group" style="float: right;">
