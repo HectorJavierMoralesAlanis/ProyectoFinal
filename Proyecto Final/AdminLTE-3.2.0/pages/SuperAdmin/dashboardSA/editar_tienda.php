d55a876d662bfd92becd293931241811108eed2d
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 00:44:38 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -1,6 +1,7 @@
 <?php
-    echo "hola";
-    echo "<h2 style='float:right;'>$tienda[nombre]</h2>";
+    echo "<h2 style='float:right;'>hola</h2><br>";
+    echo "<h2 style='float:right;'>$tienda[nombre]</h2><br>";
+    echo "<h2 style='float:right;'>$tienda[estado]</h2>";
 ?>
 
 <html>
