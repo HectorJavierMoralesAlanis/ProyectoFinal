65c9d268b7cccf6707f5694021b7dac2e2dc8e69
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 03:17:35 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -118,7 +118,7 @@
                                             <label>
                                                 Activa:
                                                 <div class="custom-control custom-radio">
-                                                <input class="custom-control-input" type="radio" id="estado" name="estado" value="on">
+                                                <input class="custom-control-input" type="radio" id="estado" name="estado">
                                                 <label for="customRadio4" class="custom-control-label">ACTIVADA</label>
                                                 </div>
                                                 <div class="custom-control custom-radio">
