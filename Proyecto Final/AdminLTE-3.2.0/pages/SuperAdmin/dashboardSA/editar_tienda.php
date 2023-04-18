e983fe9d6045a4d4b133ae758506afcd3fbe1955
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 04:15:02 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -117,7 +117,7 @@
                                             <label>
                                                 Estado:
                                             </label>
-                                            <?php if($tiendas['estado']=='Activado'){?>
+                                            <?php if($tiendas['estado']==='Activado'){?>
 
                                             <div class="custom-control custom-radio">
                                                 <input class="custom-control-input" type="radio" id="activo" name="estado" checked> 
@@ -130,7 +130,7 @@
                                                 <input class="custom-control-input custom-control-input-danger" type="radio" id="desactivado" name="estado" checked>
                                                 <label for="desactivado" class="custom-control-label">DESACTIVADA</label>
                                             </div>
-                                            
+
                                             <?php }?>
                                         </div>
                                     <?php }?>
