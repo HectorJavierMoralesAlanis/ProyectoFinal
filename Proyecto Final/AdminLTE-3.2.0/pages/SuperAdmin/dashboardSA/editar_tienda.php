4c423ee75d107ed8eec3b096377d920dc44c0407
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-18 04:51:03 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -11,8 +11,7 @@
             $dao2=new DAO();
 
             $consulta2="UPDATE tienda SET nombre = :nombre, estado = :estado "."WHERE id=1";
-            $id=$_GET['id'];
-
+            
             $parametros2= array("nombre"=>"$_POST[nombre]","estado"=>"$_POST[estado]");
 
             $resultado=$dao2->insertarConsulta($consulta2,$parametros2);
