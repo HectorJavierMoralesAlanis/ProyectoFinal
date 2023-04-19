443d0b5c0218640e083ddbe814ff04e8a3d280e2
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 03:23:21 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
@@ -3,8 +3,9 @@ include_once('../../PDO/DAO.php');
 
 $dao = new DAO();
 
-$pro = $_GET['pro'];
-$consulta = "SELECT *"."FROM categoria";
+$id = $_GET['id'];
+echo $id;
+$consulta = "SELECT * FROM categoria";
 $parametros = array("id"=>$pro);
 $categorias = $dao->ejecutarConsulta($consulta,$parametros);
 
