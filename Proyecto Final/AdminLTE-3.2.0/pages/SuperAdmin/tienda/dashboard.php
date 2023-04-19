18db40198bd2f02bcc0e22b772fe89e28e9c7b3b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 17:24:50 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/dashboard.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/dashboard.php
@@ -37,7 +37,7 @@ $inv=count($arrcategorias);
 
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Logo --->
-        <a href="../../dashboardSA/dashboard.php" class="brand-link">
+        <a href="../dashboardSA/dashboard.php" class="brand-link">
             <img src="./lemur.png"  class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Admin</span>
         </a>
@@ -65,28 +65,28 @@ $inv=count($arrcategorias);
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../inventario/inventario.php?id=<?php echo $id?>" class="nav-link">
+                            <a href="./inventario/inventario.php?id=<?php echo $id?>" class="nav-link">
                                 <p>
                                     Inventario
                                 </p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../usuarios/usuarios.php?id=<?php echo $id?>" class="nav-link">
+                            <a href="./usuarios/usuarios.php?id=<?php echo $id?>" class="nav-link">
                                 <p>
                                     Usuarios
                                 </p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../categorias/categoria.php?id=<?php echo $id?>" class="nav-link">
+                            <a href="./categorias/categoria.php?id=<?php echo $id?>" class="nav-link">
                                 <p>
                                     Categorias
                                 </p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../venta/venta.php?id=<?php echo $id?>" class="nav-link">
+                            <a href="./venta/venta.php?id=<?php echo $id?>" class="nav-link">
                                 <p>
                                     Realizar Venta
                                 </p>
@@ -100,7 +100,7 @@ $inv=count($arrcategorias);
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../../login.php" class="nav-link">
+                            <a href="../login.php" class="nav-link">
                                 <p>
                                     Cerrar sesion
                                 </p>
