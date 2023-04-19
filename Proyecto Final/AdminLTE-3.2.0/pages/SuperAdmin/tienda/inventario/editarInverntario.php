f71962c4cd10d22a856d957beb64b17b36332afe
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 15:52:55 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/editarInverntario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/editarInverntario.php
@@ -20,6 +20,7 @@ if(isset($_POST['codigo_inventario'], $_POST['nombre_producto'], $_POST['precioP
     $fecha=date('Y-m-d H:i:s');
     $id2=$_GET['id'];
     $consulta="UPDATE inventario SET codigo=:codigo,nombre=:nombre,fecha=:fecha,precioProducto=:precioProducto,categoria=:categoria,stock=:stock WHERE codigo=:idP";
+    
     $parametros=array("codigo"=>"$_POST[codigo_inventario]","nombre"=>"$_POST[nombre_producto]","fecha"=>$fecha,"precioProducto"=>"$_POST[precioProducto_inventario]","cateogria"=>"$_POST[id_categoria]","stock"=>"$_POST[stock]","idP"=>$id2);
 
     $resultados=$dao->insertarConsulta($consulta,$parametros);
