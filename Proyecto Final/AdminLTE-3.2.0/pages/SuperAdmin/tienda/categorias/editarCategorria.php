29f43b7c85ab74ebda5d545c807118d3be0eee59
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 03:35:55 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
@@ -9,6 +9,7 @@ echo $id;
 $consulta="SELECT nombre,descripcion FROM categoria WHERE id=:id";
 $parametros=array("id"=>$id);
 $resultados=$dao->ejecutarConsulta($consulta,$parametros);
+
 if($resultados>=0){
     echo "si";
     echo $resultados[0];
