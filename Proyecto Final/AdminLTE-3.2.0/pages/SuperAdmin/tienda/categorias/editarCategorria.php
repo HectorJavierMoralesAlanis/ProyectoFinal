7e9dcdec32ac675c14dc05623158d7a28776b12b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 07:42:26 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
@@ -30,8 +30,9 @@ if(isset($_POST['nombre'],$_POST['descripcion'])){
     $parametros2 = array("nombre"=>"$_POST[nombre]","descripcion"=>"$_POST[descripcion]","fecha"=>$fecha,"id2"=>$id2);
     $resultados2 = $dao2->insertarConsulta($consulta2,$parametros2);
     if($resultados2>=0){
-        echo $id3;
-        //header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php?id=$id3");
+        foreach($resultados as $id=> $tie){
+        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php?id=$tie[id]");
+        }
     }else{
         echo "error";
     }
