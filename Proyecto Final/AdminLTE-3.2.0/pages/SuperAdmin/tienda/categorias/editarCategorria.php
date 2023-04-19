09bb78c5bd726677ba29f26790804bfc3ffbd196
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 04:11:29 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
@@ -21,17 +21,14 @@ if($resultados>=0){
 //$r = searchCAT($id); //Se realiza una busqueda en la base de datos 
 //Se revisa que la variable se encuentre definida
 if(isset($_POST['nombre'],$_POST['descripcion'])){
-    echo $_POST['nombre'];
     $dao2 = new DAO(); 
     $fecha=date('Y-m-d H:i:s');
     $id2 = $_GET['id'];
-    $consulta2 = "UPDATE categoria SET nombre = :nombre, descripcion=:descripcion, fecha=:fecha WHERE id=1";
-    
-    $parametros2 = array("nombre"=>"$_POST[nombre]","descripcion"=>"$_POST[descripcion]","fecha"=>$fecha);
+    $consulta2 = "UPDATE categoria SET nombre = :nombre, descripcion=:descripcion, fecha=:fecha WHERE id=:id2";
+    $parametros2 = array("nombre"=>"$_POST[nombre]","descripcion"=>"$_POST[descripcion]","fecha"=>$fecha,"id2"=>$id2);
     $resultados2 = $dao2->insertarConsulta($consulta2,$parametros2);
     if($resultados2>=0){
-        echo $_POST['descripcion'];
-        //header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php?id=$id");
+        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php?id=$id");
     }else{
         echo "error";
     }
