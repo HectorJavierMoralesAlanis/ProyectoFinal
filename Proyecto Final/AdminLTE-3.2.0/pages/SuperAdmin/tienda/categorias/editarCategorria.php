61f25b9a1d0f1c6279821ff89eb9f7832200dd67
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 04:04:25 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/editarCategorria.php
@@ -26,8 +26,8 @@ if(isset($_POST['nombre'],$_POST['descripcion'])){
     $dao2 = new DAO(); 
     $fecha=date('Y-m-d H:i:s');
     $id2 = $_GET['id'];
-    $consulta2 = "UPDATE categoria SET nombre = :nombre,descripcion=:descripcion,fecha=:fecha"."WHERE id=1";
-    $parametros2 = array("nombre"=>"$_POST[nombre]","descripcion"=>"$_POST[descripcion]","fecha"=>"fecha");
+    $consulta2 = "UPDATE categoria SET nombre = :nombre, descripcion=:descripcion, fecha=:fecha"."WHERE id=1";
+    $parametros2 = array("nombre"=>"$_POST[nombre]","descripcion"=>"$_POST[descripcion]","fecha"=>$fecha);
     $resultados2 = $dao->insertarConsulta($consulta2,$parametros2);
     if($resultados2>=0){
         echo $_POST['descripcion'];
