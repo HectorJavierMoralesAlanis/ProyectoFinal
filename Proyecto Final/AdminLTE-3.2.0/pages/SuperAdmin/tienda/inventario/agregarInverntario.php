7f4f5b030ebdfe0f54bcda7cd0257d997341503e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 05:31:05 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/agregarInverntario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/agregarInverntario.php
@@ -210,6 +210,7 @@ if(isset($_POST['codigo_inventario'], $_POST['nombre_producto'], $_POST['precioP
                                             <?php foreach($user_access as $row){?>
                                                 <option value="<?php echo $row['nombre']?>"><?php echo $row['descripcion']?></option>
                                             <?php }?>
+                                            
                                         </select>
                                     </div>
                                     <div class="form-group">
