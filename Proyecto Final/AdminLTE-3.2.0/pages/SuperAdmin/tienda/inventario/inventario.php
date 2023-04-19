6752bcbeeb7c2bd05d22ba05768eab9d7bf75aec
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 05:41:40 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/inventario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/inventario.php
@@ -174,7 +174,7 @@ if(isset($_GET['id'])){
                                         <td><?php echo $invetario['nombre']; ?></td>
                                         <td><?php echo $invetario['fechaA']; ?></td>
                                         <td><?php echo $invetario['precioProducto']; ?></td>
-                                        <td><?php echo $invetario['categoria']; ?></td>
+                                        <td><?php echo $invetario['categoria'];?></td>
                                         <td><?php echo $invetario['stock']; ?></td>
                                         <td class="align-middle"><a href="./editarInverntario.php?id=<?php echo $invetario['codigo']?>" method="POST" class="btn btn-warning btn-block btn-sm" >EDITAR</a></td>
                                         <td class="align-middle"><a href="./inventario.php?id=<?php echo($invetario['id']); ?>" class="btn btn-danger btn-block btn-sm" onClick="wait();">ELIMINAR</a></td>
