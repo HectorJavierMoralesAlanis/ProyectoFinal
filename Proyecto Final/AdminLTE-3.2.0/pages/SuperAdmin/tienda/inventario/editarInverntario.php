4016fc3ebf2ec1f5192f50637f3d83fa50ba1e6b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 15:39:38 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/editarInverntario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/editarInverntario.php
@@ -28,7 +28,6 @@ if(isset($_POST['codigo_inventario'], $_POST['nombre_producto'], $_POST['precioP
         foreach($resultados as $id =>$tie){
         header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/inventario.php?id=$tie[tiendaId]");
         }
-
     }else{
         echo "error";
     }
