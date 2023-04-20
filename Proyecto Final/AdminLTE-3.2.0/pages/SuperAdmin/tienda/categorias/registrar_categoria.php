25892ed840cc9f7e4937a6ede5acea888fbdf812
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-20 00:33:34 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/registrar_categoria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/registrar_categoria.php
@@ -76,6 +76,7 @@ if(isset($_POST['nombre'], $_POST['descripcion'])){
                                 </p>
                             </a>
                         </li>
+                        
                         <li class="nav-item">
                             <a href="../inventario/inventario.php?id=<?php echo $_GET['id']?>" class="nav-link">
                                 <p>
@@ -83,7 +84,7 @@ if(isset($_POST['nombre'], $_POST['descripcion'])){
                                 </p>
                             </a>
                         </li>
-                        
+
                         <li class="nav-item">
                             <a href="../usuarios/usuarios2.php?id=<?php echo $_GET['id']?>" class="nav-link">
                                 <p>
