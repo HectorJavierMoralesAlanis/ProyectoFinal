76df2509b04b53035f4f453b5a2f684dfe5e7199
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 20:00:16 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/categoria.php
@@ -65,7 +65,7 @@ if(isset($_GET['id'])){
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-header">Opciones</li>
                         <li class="nav-item">
-                            <a href="../dashboard.php?id=<?php echo $l['id']?>" class="nav-link">
+                            <a href="../dashboard.php?id=<?php echo $id?>" class="nav-link">
                                 <p>
                                     Dashboard
                                 </p>
