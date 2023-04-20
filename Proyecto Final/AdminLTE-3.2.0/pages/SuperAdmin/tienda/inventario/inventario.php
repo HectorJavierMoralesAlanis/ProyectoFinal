11d010898a43c1afee7919cca2abfbe75ba6d96b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-20 00:42:15 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/inventario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/inventario/inventario.php
@@ -148,7 +148,6 @@ $productos=$dao->ejecutarConsulta($consulta,$parametros);
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        
                                     <!-- EXTRAE TODOS LOS DATOS DE LA TABLA EN LA BASE DE DATOS Y LOS MUESTRA AQUI -->
                                     <?php foreach ($productos as $invetario) { ?>
                                     <tr>
