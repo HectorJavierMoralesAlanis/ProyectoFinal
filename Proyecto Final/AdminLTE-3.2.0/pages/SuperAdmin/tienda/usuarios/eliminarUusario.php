02713a2fb097cef1fed7374e63adb9168c84ab7a
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 21:41:59 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
@@ -4,6 +4,7 @@ $id=$_GET['id'];
 $dao2=new DAO();
 $consulta2="DELETE FROM usuarios WHERE tiendaId=:idU";
 $parametros2=array("idU"=>$id);
+
 $resultados=$dao->insertarConsulta($consulta2,$parametros2);
 if($resultados>=0){
     foreach($usuarios as $id => $l){
