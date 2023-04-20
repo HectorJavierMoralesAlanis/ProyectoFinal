6d8b2ff787518842bc3086f58543a9d901fe584e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 21:06:58 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php
@@ -1,5 +1,24 @@
 <?php 
+include_once("../../PDO/DAO.php");
 $id=$_GET['id'];
+$dao =new DAO();
+$consulta="SELECT * FROM usuarios WHERE tiendaId=:id";
+$parametros=array("id"=>$id);
+$usuarios=$dao->ejecutarConsulta($consulta,$parametros);
+
+if(isset($_POST['borrar'])){
+    $dao2=new DAO();
+    $consulta2="DELETE FROM usuarios WHERE id=:idU";
+    foreach($usuarios as $id => $usuario){
+        $parametros2=array("idU"=>$usuario['id']);
+    }
+    $resultado=$dao->insertarConsulta($consulta2,$parametros2);
+    if($resultados>=0){
+        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$id");
+    }else{
+        echo "error";
+    }
+}
 ?>
 <html>
 <head>
