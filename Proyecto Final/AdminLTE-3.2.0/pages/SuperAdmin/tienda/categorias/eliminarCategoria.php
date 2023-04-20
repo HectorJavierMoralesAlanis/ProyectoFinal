ecd22bb03c5d11cd783157d8118c6968d71fc2e6
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 23:26:41 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/eliminarCategoria.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/categorias/eliminarCategoria.php
@@ -10,6 +10,7 @@ $dao2=new DAO();
 $consulta2="DELETE FROM categoria WHERE id=:idU";
 $parametros2=array("idU"=>$id);
 $resultados=$dao2->insertarConsulta($consulta2,$parametros2);
+
 if($resultados>=0){
     foreach($usuarios as $id => $l){
     header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$l[tiendaId]");
