d004e3cb37f3c8c9a0a6ff36f62a5e93ef5f6f92
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 21:41:48 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
@@ -1,22 +1,16 @@
 <?php
 include_once("../../PDO/DAO.php");
 $id=$_GET['id'];
-$dao=new DAO();
-$consulta="SELECT * FROM usuarios WHERE id=:id";
-$parametros=array("id"=>$id);
-$usuarios=$dao->ejecutarConsulta($consulta,$parametros);
-if(isset($_POST['borrar'])){
-    $dao2=new DAO();
-    $consulta2="DELETE FROM usuarios WHERE id=:idU";
-    $parametros2=array("idU"=>$id);
-    $resultados=$dao->insertarConsulta($consulta2,$parametros2);
-    if($resultados>=0){
-        foreach($usuarios as $id => $l){
-        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$l[tiendaId]");
-        }
-    }else{
-        echo "error";
+$dao2=new DAO();
+$consulta2="DELETE FROM usuarios WHERE tiendaId=:idU";
+$parametros2=array("idU"=>$id);
+$resultados=$dao->insertarConsulta($consulta2,$parametros2);
+if($resultados>=0){
+    foreach($usuarios as $id => $l){
+    header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$id");
     }
+}else{
+    echo "error";
 }
 
 ?>
