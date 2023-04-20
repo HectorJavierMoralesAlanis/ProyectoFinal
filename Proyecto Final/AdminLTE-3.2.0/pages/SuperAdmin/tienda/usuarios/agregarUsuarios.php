90b3962855388cd3e7eb84fa88e1ed14dc63bb0f
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 20:30:25 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/agregarUsuarios.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/agregarUsuarios.php
@@ -19,7 +19,7 @@ if(isset($_POST['nombre'], $_POST['apellido'], $_POST['usuario'], $_POST['passwo
 
     $resultados=$dao->insertarConsulta($consulta,$parametros);
     if($resultados>=0){
-        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/Usuario.php?id=$id");
+        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$id");
     }else{
         echo "error";
     }
