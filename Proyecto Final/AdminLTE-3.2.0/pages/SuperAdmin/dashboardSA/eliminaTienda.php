27c46607c28befb4708e5f41357f7253ac6bc1f9
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 23:18:21 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/eliminaTienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/eliminaTienda.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../../PDO/DAO.php");
+include_once("../PDO/DAO.php");
 
 $id=$_GET['id'];
 $dao2=new DAO();
@@ -7,9 +7,7 @@ $consulta2="DELETE FROM tienda WHERE id=:idT";
 $parametros2=array("idT"=>$id);
 $resultados=$dao2->insertarConsulta($consulta2,$parametros2);
 if($resultados>=0){
-    echo $id;
-    
-    //header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+    header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
 }else{
     echo "error";
 }
