448d69ede2929c6b6955dc941cbf68d35de74033
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 21:17:00 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php
@@ -17,9 +17,10 @@ if(isset($_POST['borrar'])){
         $parametros2=array("idU"=>$usuario['id']);
     }
     $resultado=$dao->insertarConsulta($consulta2,$parametros2);
-    
+
     if($resultados>=0){
-        header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=1");
+        
+        //header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=1");
     }else{
         echo "error";
     }
