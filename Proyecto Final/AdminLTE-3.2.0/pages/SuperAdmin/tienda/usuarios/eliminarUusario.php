86eaf28868747d6943282b720de0c2483ade7659
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-04-19 21:44:08 -0600

    Actualizacion2

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/eliminarUusario.php
@@ -7,9 +7,7 @@ $parametros2=array("idU"=>$id);
 
 $resultados=$dao->insertarConsulta($consulta2,$parametros2);
 if($resultados>=0){
-    foreach($usuarios as $id => $l){
-    header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=1");
-    }
+    header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/tienda/usuarios/usuarios2.php?id=$id");
 }else{
     echo "error";
 }
