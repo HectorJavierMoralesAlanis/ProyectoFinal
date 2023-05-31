da402968ef87a64694ec63d3472ff3e100f93834
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-05-30 23:51:59 -0600

    Nueva base

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/eliminaTienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/eliminaTienda.php
@@ -19,7 +19,7 @@ $resultados=$dao2->insertarConsulta($consulta2,$parametros2);
 //Condicional donde si resultados regresa 0 o mas valores
 if($resultados>=0){
     //Funcion para cambiar la url
-    header("Location: http://134.122.77.182/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
+    header("Location: http://64.226.114.50/Proyecto%20Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/dashboard.php");
 }else{
     //Mensaje de error
     echo "error";
