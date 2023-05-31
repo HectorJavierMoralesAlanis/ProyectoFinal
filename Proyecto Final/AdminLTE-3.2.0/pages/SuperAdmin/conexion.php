da6990c930932484839be469cf1c344ae8933481
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-05-30 22:16:47 -0600

    Nueva base

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/conexion.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/conexion.php
@@ -1,7 +1,7 @@
 <?php
     try{
         //Creacion de la variable pdo donde se guardara la conexion a la base de datos
-        $pdo = new PDO('mysql:host=localhost;dbname=proyecto','admin','2e19c301ddae0c83a59446303955909e093fd240fe36561b');
+        $pdo = new PDO('mysql:host=localhost;dbname=proyecto','admin','51f22d5a340ca74312b5e2e53a6ee3997d024c445932a066');
     }catch(PDOException $e){
         echo 'Error al conectarnos: '.$e->getMessage();
     }
