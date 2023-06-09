142202aaba47a87cfda5707b48c3f86c93c61920
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-09 00:32:31 -0600

    Version Final

--- Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
+++ Proyecto Final/AdminLTE-3.2.0/pages/SuperAdmin/dashboardSA/editar_tienda.php
@@ -71,7 +71,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="../dashboardSA/editar_tienda.php" class="nav-link">Editar Tienda</a>
+                <a href="#" class="nav-link">Editar Tienda</a>
             </li>
         </ul>
     </nav>
